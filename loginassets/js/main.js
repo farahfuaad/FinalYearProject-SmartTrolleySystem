598a9f280b8b473ba61d33aa0777cb5ddba46340
farah fuaad <farah.fuaad@gmail.com>
2021-09-23 00:59:38 +0800

    updated the login page

--- loginassets/js/main.js
+++ loginassets/js/main.js
@@ -1,19 +1,7 @@
 /*===== LOGIN SHOW and HIDDEN =====*/
-const signUp = document.getElementById('sign-up'),
-    signIn = document.getElementById('sign-in'),
+const 
     loginIn = document.getElementById('login-in'),
     loginUp = document.getElementById('login-up')
-
-
-signUp.addEventListener('click', ()=>{
-    // Remove classes first if they exist
-    loginIn.classList.remove('block')
-    loginUp.classList.remove('none')
-
-    // Add classes
-    loginIn.classList.toggle('none')
-    loginUp.classList.toggle('block')
-})
 
 signIn.addEventListener('click', ()=>{
     // Remove classes first if they exist
